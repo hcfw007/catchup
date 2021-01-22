3755877ea34665d50375b45446787cdb9c3eae1b
hcfw007 <brotherstyx@gmail.com>
2021-01-22 18:17:15 +0800

    feat: display child process output

--- src/restartProject.js
+++ src/restartProject.js
@@ -7,7 +7,19 @@ const startProject = function (project, config) {
   let projectPath = path.join(config.rootPath, project.name)
 
   let install = execSync(`cnpm install`, { cwd: projectPath })
-  return exec('node app.js', { cwd: projectPath })
+  let run = exec('node app.js', { cwd: projectPath })
+  run.stdout.on('data', (data) => {
+    console.log(`childprocess_stdout: ${data}`)
+  })
+
+  run.stderr.on('data', (data) => {
+    console.error(`childprocess_stderr: ${data}`)
+  })
+
+  run.on('close', (code) => {
+    console.log(`child process exited with code ${code}`)
+  })
+  return run
 }
 
 const restartProject = function (project, config, process) {
